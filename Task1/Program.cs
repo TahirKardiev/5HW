9bb60f78b7eba42fbef708390152f5a2177d4eec
Andrew <mr.fesenko@inbox.ru>
2022-08-31 11:26:51 +0300

    added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿void PrintArray(double[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+Console.WriteLine();
+}
+}
+void FillArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(1,10);
+        matrix[i,j] = matrix[i,j] / 2;
+    }
+}
+}
+double[,] matrix = new double[3, 4];
+
+FillArray(matrix);
+PrintArray(matrix);
