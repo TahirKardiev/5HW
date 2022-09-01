3e13045e7e935b5accf2d32eb69d68d80569c312
Andrew <mr.fesenko@inbox.ru>
2022-09-01 17:24:27 +0300

    added task3

---
+++ Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿void PrintArray(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+Console.WriteLine();
+}
+}
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(1,10);
+    }
+}
+}
+int[,] matrix = new int[3, 4];
+
+FillArray(matrix);
+PrintArray(matrix);
+
+Console.WriteLine();
+
+void GetArithmeticMean (int[,] matrix)
+{
+    double[] AM = new double [matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(1); i++)
+{
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+       AM[i]+=matrix[j,i];
+    }
+    AM[i]/= matrix.GetLength(0);
+    Console.Write($"{AM[i]}; ");
+}
+}
+
+GetArithmeticMean(matrix);
